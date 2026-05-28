77d3ed467e3a7b91b1ef8c850bc900259b32e283
PawaniMekhala <pawanimekhala3@gmail.com>
2026-05-28 21:52:37 +0530

    feat: add database connnection to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,32 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import connectDB from "./config/db.js";
 import productRoutes from "./routes/products.js";
 
 
 dotenv.config();
 
-
-
 const app = express();
 
-
+// Middleware
 app.use(cors());
 app.use(express.json()); // parse JSON bodies
 
+// Connect Database
+connectDB();
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
+// Routes
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
     });
-})
-.catch((error) => {
-    console.log(error);
-  });
