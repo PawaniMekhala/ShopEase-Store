8bf95fa13815fcce0e33a34c31c2ddd90ee0d9a9
PawaniMekhala <pawanimekhala3@gmail.com>
2026-05-30 23:17:07 +0530

    feat: add product route to get products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-// const ctrl = require('../controllers/productController');
-
-// router.get('/', ctrl.getAllProducts);          // Read (list) + optional search
-// router.get('/:id', ctrl.getProductById);       // Read single
-// router.post('/', ctrl.createProduct);          // Create
-// router.put('/:id', ctrl.updateProduct);        // Update
-// router.delete('/:id', ctrl.deleteProduct);     // Delete
-
-// module.exports = router;
-
 import express from "express";
+import {
+  getProducts
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Products Route Working",
-  });
-});
+router.get("/", getProducts);
 
 export default router;
