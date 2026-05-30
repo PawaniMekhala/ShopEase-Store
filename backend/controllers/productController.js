5b6c9cfc1c53fe3d39d90e2fa652919f663ec712
PawaniMekhala <pawanimekhala3@gmail.com>
2026-05-30 23:17:57 +0530

    feat: add controller function to fetch add product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,3 +14,15 @@ export const getProducts = async (req, res) => {
   }
 };
 
+// Add product
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
