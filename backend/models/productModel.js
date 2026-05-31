6d2375f80dd312e2fd623e41d4c7d113dcd16651
PawaniMekhala <pawanimekhala3@gmail.com>
2026-05-31 22:33:59 +0530

    refactor: modify product model details

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,28 +5,46 @@ const productSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
 
     price: {
       type: Number,
       required: true,
+       min: 0,
     },
 
     description: {
       type: String,
+      default: "",
     },
 
     image: {
       type: String,
+      required: true,
     },
 
     category: {
       type: String,
+      required: true,
+    },
+
+    color: {
+      type: String,
+      default: "",
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
     },
 
     stock: {
       type: Number,
       default: 0,
+      min: 0,
     },
   },
   {
